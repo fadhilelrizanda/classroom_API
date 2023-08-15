be0988655de31f273b677e53bc94cdbae62ae093
fadhilelrizanda <fadhilelrizanda@gmail.com>
2023-08-16 00:46:22 +0700

    api added

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ database.on("error", (error) => {
 });
 
 database.once("connected", () => {
-  console.log("Database Connected");
+  console.log("Database Connected!");
 });
 const app = express();
 app.use(cors());
