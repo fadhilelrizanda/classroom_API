19a3c39fc5335ba912ae86c37a39af01d6a3fa32
fadhilelrizanda <fadhilelrizanda@gmail.com>
2024-06-07 15:10:45 +0700

    Adding cors

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
 const routes = require("./routes/routes");
 const roomstat = require("./routes/roomstat");
 const classRoutes = require("./routes/class");
 const acStatRoutes = require("./routes/acstat");
 const relayStatRoutes = require("./routes/relaystat");
 const socketRoutes = require("./routes/socketstat");
-const express = require("express");
-const mongoose = require("mongoose");
-require("dotenv").config();
-const mongoString = process.env.DATABASE_URL;
 
+const mongoString = process.env.DATABASE_URL;
 mongoose.connect(mongoString);
 const database = mongoose.connection;
-
-const cors = require("cors");
 
 database.on("error", (error) => {
   console.log(error);
@@ -21,21 +21,32 @@ database.on("error", (error) => {
 database.once("connected", () => {
   console.log("Database Connected!");
 });
+
 const app = express();
-app.use(cors());
+
+// Configure CORS
+const corsOptions = {
+  origin: "*", // Allow all origins (you can specify a specific origin instead of '*')
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
 
-app.use(express.json());
-// const routes = require("./rout es/routes");
 app.use("/roomstat", roomstat);
 app.use("/acstat", acStatRoutes);
 app.use("/api", routes);
 app.use("/class", classRoutes);
 app.use("/relaystat", relayStatRoutes);
 app.use("/socketstat", socketRoutes);
-app.listen(5000, () => {
-  console.log(`Server Started at ${5000}`);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server Started at ${PORT}`);
 });
