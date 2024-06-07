56055ccaeb454022a933eab9f4459f175020b4fb
fadhilelrizanda <fadhilelrizanda@gmail.com>
2024-06-07 17:15:10 +0700

    cors update 1

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ database.on("error", (error) => {
 });
 
 database.once("connected", () => {
-  console.log("Database Connected!");
+  // console.log("Database Connected!");
 });
 
 const app = express();
