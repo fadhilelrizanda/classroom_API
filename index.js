1b422d064abe79d78d4b9b9beb134218b783eb2d
fadhilelrizanda <fadhilelrizanda@gmail.com>
2024-06-07 17:33:23 +0700

    t1

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.options("*", cors(corsOptions)); // Enable preflight requests for all routes
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
